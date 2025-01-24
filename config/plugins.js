2a1f8918d914e95fb16ac04ab724e38a14f18063
Akshatabhor <akshatabhor6212@gmail.com>
2025-01-24 16:47:00 +0530

    Second Commit

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = {
+    'users-permissions': {
+      config: {
+        grant: {
+          github: {
+            clientId: 'Ov23liCQ8Cjj3ki5yRNK',
+            clientSecret: '5724534d73e9728271d658b2bd7c58c1fad16df7',
+            callback: 'http://localhost:1337/connect/github/callback'
+          }
+        }
+      }
+    }
+  };
+  
